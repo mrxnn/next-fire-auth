5ba548aa517d500486277e3ce55ea4e2767872b2
Mayura Ramanayaka <mayura.ramanayake@gmail.com>
2021-07-11 19:49:33 +0530

    CRA loading skeleton for account page

--- pages/account.js
+++ pages/account.js
@@ -1,6 +1,14 @@
-import { useContext } from "react";
-import { useRouter } from "next/router";
-import { Avatar, Button, Flex, Heading, Spinner, Text } from "@chakra-ui/react";
+import { useContext, useEffect } from "react";
+import {
+  Avatar,
+  Button,
+  Flex,
+  Heading,
+  Skeleton,
+  SkeletonCircle,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import nookies from "nookies";
 import { Layout } from "../components/Layout";
 import { AuthContext } from "../context/auth";
@@ -17,15 +25,21 @@ export async function getServerSideProps(context) {
 
 export default function Account() {
   const { user, loading, signOut } = useContext(AuthContext);
-  const router = useRouter();
 
   return (
     <Layout>
-      {loading && <Spinner />}
+      {loading && (
+        <Stack align="center" mt="20" spacing="4">
+          <SkeletonCircle size="24" mb="4" />
+          <Skeleton height="30px" w="400px" />
+          <Skeleton height="12px" w="400px" />
+          <Skeleton height="20px" w="400px" />
+        </Stack>
+      )}
 
       {user && (
-        <Flex align="center" direction="column">
-          <Avatar src={user?.photoURL} size="lg" mb="4" />
+        <Flex align="center" direction="column" mt="20">
+          <Avatar src={user?.photoURL} size="xl" mb="4" />
           <Heading size="lg">{user?.displayName}</Heading>
           <Text decoration="underline">{user?.email}</Text>
           <Button
